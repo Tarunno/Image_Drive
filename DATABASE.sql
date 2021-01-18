43884840202ce65363f56e4a4a61d4842b26500c
Mahadi Hasan Tarunno <47926003+Tarunno@users.noreply.github.com>
2021-01-18 23:05:56 +0600

    Add files via upload

---
+++ DATABASE.sql
@@ -0,0 +1,161 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 18, 2021 at 04:40 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `photos`
+--
+
+CREATE TABLE `photos` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `photo` varchar(256) NOT NULL,
+  `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `photos`
+--
+
+INSERT INTO `photos` (`id`, `user_id`, `photo`, `uploaded_at`) VALUES
+(16, 10, 'imagepexels-ovan-576906005912fd258b.jpg', '2021-01-18 13:46:23'),
+(20, 10, 'image128121366_3384791791642731_1735104484907745604_o600598a1a26ad.jpg', '2021-01-18 14:18:09'),
+(22, 10, 'imageforest-mountains-fog-clouds-97546005aae324a49.jpg', '2021-01-18 15:36:03'),
+(23, 10, 'imagepexels-adrianna-calvo-3925866005aae35f229.jpg', '2021-01-18 15:36:03'),
+(24, 10, 'imagepexels-photo-15610206005aaf21abdd.jpeg', '2021-01-18 15:36:18'),
+(25, 10, 'image127016196_747845269147344_7906298144738071190_o6005ab02a2cec.jpg', '2021-01-18 15:36:34'),
+(26, 10, 'imagedolly6005ab1706990.jpeg', '2021-01-18 15:36:55');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(256) DEFAULT NULL,
+  `last_name` varchar(256) DEFAULT NULL,
+  `image` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`id`, `user_id`, `first_name`, `last_name`, `image`) VALUES
+(2, 10, 'Mahadi', 'Hasan', 'profile_image_Tarunno_10.jpg'),
+(4, 12, 'Alexi', 'Michel', 'profile_image_Molly_12.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(256) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `varified` varchar(256) NOT NULL,
+  `token` longtext NOT NULL DEFAULT 'unauthorized'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`, `varified`, `token`) VALUES
+(10, 'Tarunno', 'mahadihasantarunno@gmail.com', '$2y$10$3QptcV8lTri7rTS0VgyT0eC/FTMskHZIh2y1cfIU3CbvvEHmWBU02', '2021-01-13 09:01:10', '0', 'Tarunno5ffeb6d6d57428.90253041ceb8b55b580e5ff770e1d081e78695e5'),
+(12, 'Molly', 'molly@fakemail.com', '$2y$10$IIfpKKeBXs4YGG9ZjMCv0O/GLdm8ygWMahTfKfhQQbQh2oDSEiygq', '2021-01-17 18:31:19', '0', 'Molly60048277bf29d9.33427005e0c1cfc885a8cbce1748bc4fc54eb1c7');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `photos`
+--
+ALTER TABLE `photos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `profile_ibfk_1` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `photos`
+--
+ALTER TABLE `photos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `photos`
+--
+ALTER TABLE `photos`
+  ADD CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `profile_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
