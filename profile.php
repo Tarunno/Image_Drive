e554d41b249ed9480641b47590424116a51dfa01
Mahadi Hasan Tarunno <47926003+Tarunno@users.noreply.github.com>
2021-01-20 22:02:49 +0600

    Update profile.php

--- profile.php
+++ profile.php
@@ -3,14 +3,14 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css"  />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="css/style.css">
-        <title>PHP JS | profile</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css"  />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
+	<title>PHP JS | profile</title>
     </head>
-	<body>
-		<!-- Logout option -->
+    <body>
+	<!-- Logout option -->
         <form class="logout-btn">
             <?php
                 if(isset($_SESSION["username"])){
@@ -19,27 +19,27 @@
             ?>
         </form>
 
-		<!-- Showcase -->
-		<div class="showcase">
-			<h1 class="heading"> PHP JS CURD OOP | <span style="color: crimson;">profile</span> </h1>
-			<p class="connection-error"> <?php echo $conn_error; ?></p>
-		</div>
-		<div class="profile">
-			<div class="profile-info">
-                <!-- Profle picture -->
-				<div class="image">
-                    <i id="change-image-btn" class="fas fa-cog"></i>
-				</div>
-                <!-- username, fullname and email -->
-				<div class="info">
-					<div class="info-inner">
-						<h3 class="full-name"></h3><i id="change-name-btn" class="fas fa-cog"></i><br>
-						<h5 class="username"></h5><br>
-						<h5 class="email"></h5>
-					</div>
+	<!-- Showcase -->
+	<div class="showcase">
+		<h1 class="heading"> PHP JS CURD OOP | <span style="color: crimson;">profile</span> </h1>
+		<p class="connection-error"> <?php echo $conn_error; ?></p>
+	</div>
+	<div class="profile">
+		<div class="profile-info">
+			<!-- Profle picture -->
+			<div class="image">
+				<i id="change-image-btn" class="fas fa-cog"></i>
+			</div>
+			<!-- username, fullname and email -->
+			<div class="info">
+				<div class="info-inner">
+					<h3 class="full-name"></h3><i id="change-name-btn" class="fas fa-cog"></i><br>
+					<h5 class="username"></h5><br>
+					<h5 class="email"></h5>
 				</div>
 			</div>
 		</div>
+	</div>
         <div class="my-timeline">
             <h1>My photos</h1>
             <div class="upload-section">
@@ -52,9 +52,9 @@
             </div>
         </div>
 
-		<!-- Scripts -->
-		<script type="text/javascript" src="javaScript/app.js"></script>
-		<script type="text/javascript" src="javaScript/profile.js"></script>
+	<!-- Scripts -->
+	<script type="text/javascript" src="javaScript/app.js"></script>
+	<script type="text/javascript" src="javaScript/profile.js"></script>
         <script type="text/javascript" src="javaScript/dragDrop.js"></script>
-	</body>
+    </body>
 </html>
